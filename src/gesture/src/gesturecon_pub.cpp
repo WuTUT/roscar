6d043b3f6c531981bf235ad72bd3cca5f0dd3bcb
WuTUT <762641171@qq.com>
2018-01-03 18:15:06 +0800

    as

--- src/gesture/src/gesturecon_pub.cpp
+++ src/gesture/src/gesturecon_pub.cpp
@@ -1,8 +1,10 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include "opencv2/opencv.hpp"
+
 using namespace cv;
 using namespace std;
 
@@ -16,20 +18,8 @@ int CrL = 5;    //轨迹条滑块对应的值
 int CrH = 5;
 int CbL = 5;
 int CbH = 5;
-
 int main(int argc, char *argv[])
 {
-	 
-	ros::init(argc, argv, "gesturecontrol");
-
-	
-	ros::NodeHandle n;
-
-	
-	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("gesture", 1000);
-
-	ros::Rate loop_rate(10);
-
 	Mat frame, frameROI, skinArea;
     VideoCapture capture;
     int num=0;
@@ -40,11 +30,23 @@ int main(int argc, char *argv[])
         cout<<"No camera!\n"<<endl;
         return -1;
     }
+    
 
+
+
+	ros::init(argc, argv, "gesturecon");
+
+	ros::NodeHandle n;
+
+	
+	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("gesturecontrol", 1000);
+
+	ros::Rate loop_rate(10);
+
+	
 	int count = 0;
 	while (ros::ok())
 	{
-		
 		capture >> frame;
         
         if (frame.empty())
@@ -144,16 +146,19 @@ int main(int argc, char *argv[])
         imshow("show_img", show_img);
         
         cout << "Number of Fingers is " << num << endl;
-
+        
+    
 
 
 		std_msgs::String msg;
 
 		std::stringstream ss;
-		ss << num;
+		ss <<num;
 		msg.data = ss.str();
 
 		ROS_INFO("%s", msg.data.c_str());
+
+		
 		chatter_pub.publish(msg);
 
 		ros::spinOnce();
@@ -163,43 +168,4 @@ int main(int argc, char *argv[])
 	}
 
 	return 0;
-}
-
-void skinExtract(const Mat &frame, Mat &skinArea)
-{
-    Mat YCbCr;
-    vector<Mat> planes;
-    
-    //转换为YCrCb颜色空间
-    cvtColor(frame, YCbCr, CV_RGB2YCrCb);
-    //将多通道图像分离为多个单通道图像
-    split(YCbCr, planes);
-    
-     //运用迭代器访问矩阵元素
-     MatIterator_<uchar> it_Cb = planes[1].begin<uchar>(),
-     it_Cb_end = planes[1].end<uchar>();
-     MatIterator_<uchar> it_Cr = planes[2].begin<uchar>();
-     MatIterator_<uchar> it_skin = skinArea.begin<uchar>();
-    
-    namedWindow("show",1);
-    
-    //创建轨迹条
-    createTrackbar("CrL：","show", &CrL,20);
-    createTrackbar("CrH：","show", &CrH,20);
-    createTrackbar("CbL：","show", &CbL,20);
-    createTrackbar("CbH：","show", &CbH,20);
-     
-    //人的皮肤颜色在YCbCr色度空间的分布范围: 138<=Cr<=175; 100<=Cb<=118
-     for( ; it_Cb != it_Cb_end; ++it_Cr, ++it_Cb, ++it_skin)
-     {
-     if (CrL+133 <= *it_Cr &&  *it_Cr <= CrH+170 && CbL+95 <= *it_Cb &&  *it_Cb <= CbH+112)
-     *it_skin = 255;
-     else
-     *it_skin = 0;
-     }
-     
-     //膨胀和腐蚀，膨胀可以填补凹洞（将裂缝桥接），腐蚀可以消除细的凸起（“斑点”噪声）
-     dilate(skinArea, skinArea, Mat(5, 5, CV_8UC1), Point(-1, -1),2);
-     erode(skinArea, skinArea, Mat(5, 5, CV_8UC1), Point(-1, -1),1);
-
 }
